84b8fb122b27f1874a03c0da908682e22903f9a8
Samn <151047928+Samn7@users.noreply.github.com>
2025-06-23 11:05:12 +0530

    Add files via upload

---
+++ Bawana/basiccalc.py
@@ -0,0 +1,19 @@
+# a = float(input("Enter first number: "))
+a = float(input("Enter First number: "))
+op = input("Enter operator (+, -, *, /): ")
+b = float(input("Enter second number: "))
+
+if op == "+":
+    print("Result:", a + b)
+elif op == "-":
+    print("Result:", a - b)
+elif op == "*":
+    print("Result:", a * b)
+elif op == "/":
+    if b != 0:
+        print("Result:", a / b)
+    else:
+        print("Cannot divide by zero.")
+else:
+    print("Invalid operator.")
+
